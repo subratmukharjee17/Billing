926b02b6102309c5caaea220323a61259757818f
shweta <shwetasp1612@gmail.com>
2023-11-06 23:48:55 -0500

    Added functinality to Add User in Admin module

--- Billing.Presentation/Controllers/UserController.cs
+++ Billing.Presentation/Controllers/UserController.cs
@@ -1,12 +1,50 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Billing.Presentation.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Text;
+
 
 namespace Billing.Presentation.Controllers
 {
     public class UserController : Controller
     {
+        Uri baseAddress = new Uri("http://localhost:36942/api");
+        private readonly HttpClient _httpClient;
+        public UserController() 
+        {
+            _httpClient = new HttpClient();
+            _httpClient.BaseAddress = baseAddress;
+        }
+
+        //public IActionResult AddUser()
+        //{
+        //    return View("~/Views/Users/AddUser.cshtml");
+        //}
+
+        [HttpGet]
         public IActionResult AddUser()
         {
-            return View("~/Views/Users/AddUser.cshtml");
+            return View();
         }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateUser(User userInfo)
+        {
+            string data = JsonConvert.SerializeObject(userInfo);
+            StringContent content = new StringContent(data, Encoding.UTF8, "application/json");
+            HttpResponseMessage response = await _httpClient.PostAsync(_httpClient.BaseAddress + "/User/AddUser", content);
+            if (response.IsSuccessStatusCode)
+            {
+                var successMessage = new { message = "User created" };
+                return Json(successMessage);
+            }
+            else
+            {
+                var errorMessage = new { error = "Invalid Input" };
+                return Json(errorMessage);
+            }
+        }
+
     }
 }
